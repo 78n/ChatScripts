12bbb39c0c87b4b7a173ac7da1dd228969c3072e
78n <108846385+78n@users.noreply.github.com>
2023-01-24 17:04:25 -0600

    Update FakeChat.lua

--- FakeChat.lua
+++ FakeChat.lua
@@ -230,4 +230,6 @@ end)
 
 local a = Instance.new("Hint",CoreGui)
 a.Text = "Fixed the command bar opening with every button pressed (forgot to add '()')"
-task.delay(10,a.Destroy(a))
+task.delay(10,function()
+a:Destroy()
+end)
